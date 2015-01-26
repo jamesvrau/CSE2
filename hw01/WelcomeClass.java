a7d7cae86b904137bbe4b274103b0f19a7d49139
jamesvrau <jvr217@lehigh.edu>
2015-01-26 22:24:12 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,28 @@
+//James Rau
+//Homework Assignment 1
+//
+//
+// define a class
+public class WelcomeClass{
+    
+// add main method
+    public static void main(String[ ] args) {
+        
+// print the statement specifically formatted per instructions
+    System.out.println("   -----------");
+    System.out.println("   | WELCOME |");
+    System.out.println("   -----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("<-J--V--R--2--1--7->");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v ");
+    System.out.println("Plunge boldly into the thick of life, and seize");
+    System.out.println("it where you will - make it interesting.");
+    
+  
+    //test/testtest
+    
+        
+    }
+}
