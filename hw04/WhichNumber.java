2faa241387744052adb26ab90b67527cc10677d7
jamesvrau <jvr217@lehigh.edu>
2015-02-18 00:42:30 +0000

    updated WhichNumber

--- hw04/WhichNumber.java
+++ hw04/WhichNumber.java
@@ -1,46 +1,81 @@
 //James Rau, CSE 002, 17/02/2015
-//Program for defining and guessing a number 1-10
+//Program for reducing an unknown number between 1-10
+//then guessing the answer 
 
     
 // import scanner class 
 import java.util.Scanner;
     
 // add class and main method respectively 
-public class Cookies {
+public class WhichNumber {
 public static void main (String [ ] args) {
-    Scanner input = new Scanner( System.in ); // create a Scanner
-    System.out.print( "Think of a number between 1-10 inclusive " ); 
     
-    System.out.print ( "Is your number even: " );  // print out command for user    
-    String input=Y&y&N&n; 
-
-
-
-        switch (input) {
-            case Y&y:
-                System.out.print ( "Is your number divisible by 3: " );
-                    
-           
-        }
+    System.out.print( "It won't compile, but code is here (below)... " );
+    System.out.print( "error with statement marked ** below. " );
+    
+}}
+    
+//    Scanner input = new Scanner( System.in ); // create a Scanner
+//    System.out.print( "Think of a number between 1-10 inclusive " ); 
+//  **  String yes = new String( "y"); 
+    
+//    System.out.print( "Is your number even: " );  // print out command for user    
+//
+//  **      if(input.equalsIgnoreCase(yes)) { //start to container 1, it is even
+//        
+//            
+//                System.out.print ( "Is your number divisible by 3: " );
+//                    
+//                        if(input.equalsIgnoreCase ("Y")) {
+//                        System.out.print( "You number is 6 " );  //yes divisible by 3                                 
+//                        }
+//                        
+//                        else { //not divisible by 3
+//                                   
+//                                    System.out.print ( "Is your number divisible by 4: " ); 
+//                                    if(input.equalsIgnoreCase ("y")) { //yes divisible by 4
+//                                        
+//                                            System.out.print ( "Is the number divided by 4 greater than 1?: " );
+//                                                if(input.equalsIgnoreCase ("y")) { System.out.print( "Your number is 8 " );} //divisible by 8
+//                                                else {System.out.print ( " Your number is 4 ");}
+//                                    
+//                                    } //closing bracket to divisble by 4
+//                                    
+//                                    else { //not divisible by 4
+//                                        System.out.print ( "Is it divisible by 5?: " );
+//                                                if(input.equalsIgnoreCase ("y")) { System.out.print( "Your number is 10 " );} //divsible by 5
+//                                                else {System.out.print ( " Your number is 2 ");}
+//                                        
+//                                    } //closing bracket to not divisible by 4
+                                        
+                        
+                            
+                            
+                            
+//                        }//close bracket to not divisible by 3
+                        
+                        
+            
         
-        if(input==y&Y) 
-            {    }     
-        
-        
-        else(input==n&N)
-        {   }
             
             
             
-        
+         
+         
+         
+            
+//        } //closing bracket to container 1
+ 
+  
+            
+        //**start container 2 for no to first question  
+//        else { 
+            
+//            System.out.println ("Is it divisible by 3 "); 
             
             
             
-            
-            
-            
-            
-            
-            
-        } // end of method
-    }// end of class
+//        } //closing bracket to container 2    
+//            
+//        } // end of method
+//    }// end of class
