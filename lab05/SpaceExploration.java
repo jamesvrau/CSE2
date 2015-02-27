54d0a33bea9d7e2c7ae9ce9de7be7beaf38f8d0e
jamesvrau <jvr217@lehigh.edu>
2015-02-27 00:39:26 +0000

    updated SpaceExploration

--- lab05/SpaceExploration.java
+++ lab05/SpaceExploration.java
@@ -3,30 +3,117 @@
  
 public class SpaceExploration { // add class and main method respectively 
 public static void main (String [ ] args) {
-
-int year = (int)(Math.random()*(upperBound+1) ) + baseNum;
-int year = (int)(Math.random ()*11) + 2000;
     
-
-
-switch (year) 
-
-    case 1: 
-    return;
+int year = (int)(Math.random ()*11) + 2000; //generate number [2000-2010]
     
-    case 2:
-    return; 
+System.out.print ( "Here is a timeline of space exploration events from " +year+ " to 2000 \n" ); //prepare timeline
+
+switch (year) {
+
+    case 2000: 
+        System.out.print ( "2000: First spacecraft orbits an asteroid \n" );
+    break;
     
-    case 3: 
-    return;
+    case 2001: 
+        System.out.print ( "2001: First spacecraft orbits an asteroid \n" );
+        System.out.print ( "2000: First spacecraft orbits an asteroid \n" );
+    break; 
+    
+    case 2002: 
+        System.out.print ( "2002: N/A \n" );
+        System.out.print ( "2001: First spacecraft orbits an asteroid \n" );
+        System.out.print ( "2000: First spacecraft orbits an asteroid \n" );
+    break;
+    
+    case 2003: 
+        System.out.print ( "2003: Largest infrared telescope released \n" );
+        System.out.print ( "2002: N/A \n" );
+        System.out.print ( "2001: First spacecraft orbits an asteroid \n" );
+        System.out.print ( "2000: First spacecraft orbits an asteroid \n" );
+    break;
+    
+    case 2004: 
+        System.out.print ( "2004: N/A \n" );
+        System.out.print ( "2003: Largest infrared telescope released \n" );
+        System.out.print ( "2002: N/A \n" );
+        System.out.print ( "2001: First spacecraft orbits an asteroid \n" );
+        System.out.print ( "2000: First spacecraft orbits an asteroid \n" );
+    break;
+    
+    case 2005: 
+        System.out.print ( "2005: Spacecraft collides with comet \n" );
+        System.out.print ( "2004: N/A \n" );
+        System.out.print ( "2003: Largest infrared telescope released \n" );
+        System.out.print ( "2002: N/A \n" );
+        System.out.print ( "2001: First spacecraft orbits an asteroid \n" );
+        System.out.print ( "2000: First spacecraft orbits an asteroid \n" );
+    break;
+    
+    case 2006: 
+        System.out.print ( "2006: Spacecraft returns with collections from a comet \n" );
+        System.out.print ( "2005: Spacecraft collides with comet \n" );
+        System.out.print ( "2004: N/A \n" );
+        System.out.print ( "2003: Largest infrared telescope released \n" );
+        System.out.print ( "2002: N/A \n" );
+        System.out.print ( "2001: First spacecraft orbits an asteroid \n" );
+        System.out.print ( "2000: First spacecraft orbits an asteroid \n" );
+    break;
+    
+    case 2007: 
+        System.out.print ( "2007: N/A \n" );
+        System.out.print ( "2006: Spacecraft returns with collections from a comet \n" );
+        System.out.print ( "2005: Spacecraft collides with comet \n" );
+        System.out.print ( "2004: N/A \n" );
+        System.out.print ( "2003: Largest infrared telescope released \n" );
+        System.out.print ( "2002: N/A \n" );
+        System.out.print ( "2001: First spacecraft orbits an asteroid \n" );
+        System.out.print ( "2000: First spacecraft orbits an asteroid \n" );
+    break;
+    
+    case 2008: 
+        System.out.print ( "2008: Kepler launched to study deep space \n" );
+        System.out.print ( "2007: N/A \n" );
+        System.out.print ( "2006: Spacecraft returns with collections from a comet \n" );
+        System.out.print ( "2005: Spacecraft collides with comet \n" );
+        System.out.print ( "2004: N/A \n" );
+        System.out.print ( "2003: Largest infrared telescope released \n" );
+        System.out.print ( "2002: N/A \n" );
+        System.out.print ( "2001: First spacecraft orbits an asteroid \n" );
+        System.out.print ( "2000: First spacecraft orbits an asteroid \n" );
+    break;
+    
+    case 2009: 
+        System.out.print ( "2009: N/A \n" );
+        System.out.print ( "2008: Kepler launched to study deep space \n" );
+        System.out.print ( "2007: N/A \n" );
+        System.out.print ( "2006: Spacecraft returns with collections from a comet \n" );
+        System.out.print ( "2005: Spacecraft collides with comet \n" );
+        System.out.print ( "2004: N/A \n" );
+        System.out.print ( "2003: Largest infrared telescope released \n" );
+        System.out.print ( "2002: N/A \n" );
+        System.out.print ( "2001: First spacecraft orbits an asteroid \n" );
+        System.out.print ( "2000: First spacecraft orbits an asteroid \n" );
+    break;
+    
+    case 2010: 
+        System.out.print ( "2010: SpaceX successfully sends spacecraft to orbit and back \n" );
+        System.out.print ( "2009: N/A \n" );
+        System.out.print ( "2008: Kepler launched to study deep space \n" );
+        System.out.print ( "2007: N/A \n" );
+        System.out.print ( "2006: Spacecraft returns with collections from a comet \n" );
+        System.out.print ( "2005: Spacecraft collides with comet \n" );
+        System.out.print ( "2004: N/A \n" );
+        System.out.print ( "2003: Largest infrared telescope released \n" );
+        System.out.print ( "2002: N/A \n" );
+        System.out.print ( "2001: First spacecraft orbits an asteroid \n" );
+        System.out.print ( "2000: First spacecraft orbits an asteroid \n" );
+    break;
     
     default:
-    return; 
+    break; 
     
-System.out.print ( "Number of people " );
-    
-    
-    
+
+} //close switch statement 
     
     
     
