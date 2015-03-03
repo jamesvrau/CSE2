2e563b7f2e75b034c4a39a1a2b512dd7844ea54b
jamesvrau <jvr217@lehigh.edu>
2015-03-03 17:50:30 +0000

    updated RunFactorial

--- hw06/RunFactorial.java
+++ hw06/RunFactorial.java
@@ -4,24 +4,29 @@
 
 import java.util.Scanner; // import scanner outside of class
 
-public class SmileGenerator { // add class and main method respectively 
+public class RunFactorial { // add class and main method respectively 
 public static void main (String [ ] args) {
 
 int intCheck = 0; //initialized
-int factorial =1;
+int multiple =1;
+int factorial =1; //answer
 
     Scanner in = new Scanner( System.in ); // create a Scanner for variable 'in'
     System.out.println( "Please enter an integer [9-16] : " ); 
     intCheck = in.nextInt(); //checks integer value
     
+
+
 if (intCheck<=16 && intCheck>=9) {
     
     
     while (multiple<=intCheck) {
+
         factorial = factorial*multiple;
-        System.out.println( " Input accepted: /n" + intcheck + "!=" + factorial);
-    } //close while loop   
+        multiple++;
     
+    } //close while loop   
+        System.out.println( " Input accepted: \n" + intCheck + "!= " + factorial);
 
 } //close if loop
         
